03963beed71eb2f1659734e0aca2159c9e99588f
Lama Yaseen <lsyaseens@gmail.com>
2021-03-18 17:45:05 +0300

    added a new query

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -10,6 +10,10 @@ public class QueryProcessor {
             return "someone else (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
+        } else if (query.toLowerCase().contains("ada")) {
+            return "Augusta Ada King, Countess of Lovelace (0 December 1815 – 27 November 1852) " +
+                    "was an English mathematician and writer, chiefly known for her work on " +
+                    "Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine.";
         }
         return "";
     }
